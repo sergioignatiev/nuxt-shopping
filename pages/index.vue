37ca68ff4333b0776663d1123d96abdcd1e22795
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-03 23:14:16 +0300

    add all the others

--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,12 @@
     <TheCard image="/headphones-white.png"/>
     <TheCard image="/headphones.png"/>
     <TheCard image="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"/>
-    <TheCard image="/public/headphones-logitech.png"/>
-    <TheCard image="/public/headphones-white.png"/>
-    <TheCard image="/public/headphones.png"/>
-    <TheCard image="/public/headphones-white.png"/>
-    <TheCard image="/public/headphones-logitech.png"/>
-    <TheCard image="/public/headphones-white.png"/>
-
+    <TheCard image="/headphones-logitech.png"/>
+    <TheCard image="/headphones-white.png"/>
+    <TheCard image="/headphones.png"/>
+    <TheCard image="/headphones-white.png"/>
+    <TheCard image="/headphones-logitech.png"/>
+    <TheCard image="/headphones-white.
 
     </div>
     <h1 class="text-[#eec353]  text-[40px] font-extrabold ">index page</h1>
