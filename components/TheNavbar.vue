fa41c76e68a4692fb9983a00c8c7aa3354b264a6
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-07 23:48:55 +0300

    search v-if

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -4,13 +4,16 @@
 <NuxtLink class="text-[20px] font-extrabold" v-for="n in navigation" :to="n.link" :key="n.id">{{ n.label }}</NuxtLink>
 <div class="">
 <input @change="findMatches" class="w-[300px] p-2 shadow-lg bg-slate-100" type="search" placeholder="Поиск" v-model="search">
-<p class="absolute z-10 bg-white " v-show="search">
-  <p v-for="x in findMatches" :key="x.id">
-    <NuxtLink class="flex" :to="`/id/${x.id}`" @click="search=''">
+
+<p class="absolute z-10 shadow-lg bg-white px-3 " v-if="search">
+  
+    <NuxtLink v-for="x in findMatches"   class="flex py-3 sticky top-0" :to="`/id/${x.id}`" @click="search=''" :key="x.id">
       <img class="w-[30px]" :src="x.image" :alt="x.title">
       {{ x.title }}</NuxtLink>
-  </p>
+  
 </p>
+
+
 </div>
 </nav>
 
@@ -26,6 +29,8 @@ import { useProductsStore } from '~/stores/products';
 const storeProducts=useProductsStore()
 const {products}=storeToRefs(storeProducts)
 const search=ref('')
+const wait=ref('false')
+
 const findMatches=computed(()=>{
   
   return products.value.filter(i=>i.title.toLowerCase().includes(search.value.toLocaleLowerCase()))
