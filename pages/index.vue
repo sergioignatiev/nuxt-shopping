ca1757d0f9fd0bfde6cc4071329ca630a2c6f4e2
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-08 17:06:55 +0300

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,5 @@
 <template>
-<h1 class="text-[70px] h-[100vh] bg-yellow-300  flex items-center justify-center font-extrabold ">
-<div class="rotate-12">
-<p>HE<span class="text-white">LL</span>O USER</p>
-<p>GLAD TO S<span class="text-white">EE</span> YOU<span class="text-white">!</span></p>
 
-</div>
-
-</h1>
 </template>
 
 <script setup>
