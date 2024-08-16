24356f24ee67cbf55b8b2e700c1f684daa4b657e
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-17 00:58:57 +0300

    vercel-nuxtimg

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="text-red-300">TEXT_RED</h1>
     <NuxtLink to="/profile/linguriosa">
-    <img src="/public/linguriosa.png" alt="linguriosa" class="w-[320px]">
+    <NuxtImg src="/linguriosa.png" alt="linguriosa" class="w-[320px]"/>
     </NuxtLink>
   </div>
 </template>
