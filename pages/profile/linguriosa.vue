b8a5fdeea1bdb432ee4b5126373310a1808bf56b
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-26 22:25:13 +0300

    refrech

--- pages/profile/linguriosa.vue
+++ pages/profile/linguriosa.vue
@@ -1,27 +1,13 @@
 <template>
-  <div >
-    <input ref="input" v-model="count" type="number">
-   <NuxtImg :src="w" alt="w"/>
-  </div>
+ <div class="flex flex-wrap">
+  <div v-for="p in products" :key="p.id" class="">{{ p.id }}</div>
+ </div>
 </template>
 
-<script setup>
-const count=ref(0)
-const input=ref(null)
-const w=ref('https://v3.woonuxt.com/.netlify/images?w=2800&h=1600&url=%2Fimages%2Fhero-4.jpg')
-onMounted(()=>{
-  input.value.focus()
-})
-watch(count,(n)=>{
- if(n>=11){
-    w.value='https://upload.wikimedia.org/wikipedia/commons/a/a0/Catalinamicaelaspain65.jpg'
-  }
- else if(n>9){
-    w.value="linguriosa.png"
-  }else if(n<=9){
-    w.value='https://v3.woonuxt.com/.netlify/images?w=2800&h=1600&url=%2Fimages%2Fhero-4.jpg'
-  }
-})
+<script lang="ts" setup>
+import { useProductsStore,storeToRefs } from '#imports';
+const {products}=storeToRefs(useProductsStore())
+
 </script>
 
 <style>
