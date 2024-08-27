5b636e6bb6926c9e405d1aaa610348b484b88389
SergioIgnatev <ignatevsergey17@gmail.com>
2024-08-27 15:49:44 +0300

    TheCardProps

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>
-    <h1>CONTACTS</h1>
+  <div class="flex flex-wrap gap-4">
+ <div v-for="i in filtered" :key="i" class="">
+    <div class="flex flex-wrap gap-4">
+    <div v-for="c in i" :key=c.id>
+        <TheCard
+    
+     :id="c.id"
+     :key="c.id"
+     :title="c.title"
+     :image="c.image"
+     :rating="c.rating.rate"
+     :price='c.price'
+  
+     />
+    </div>
+</div> 
+</div>
+
   </div>
 </template>
 
-<script lang="ts" setup>
-
+<script  setup>
+import { useProductsStore,storeToRefs } from '#imports';
+const {categories,products}=storeToRefs(useProductsStore())
+const filtered=computed(()=>{
+    return categories.value.map(c=>products.value.filter(i=>i.category==c))
+})
 </script>
 
 <style>
